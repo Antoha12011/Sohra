9aafdd919f4a9b6cf8ceb79138f63c0c53a52627
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-08-28 21:05:17 +0300

    Initial Commit

---
+++ Sohra/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sohra
+//
+//  Created by Антон Павлов on 28.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
